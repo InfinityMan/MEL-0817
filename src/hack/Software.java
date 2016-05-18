3985f4bffecd406af4cb41db7c576d1b708d5576
Dmig <Dmig13@ya.ru>
2016-05-18 16:08:37 +0300

    Basic software class

---
+++ src/hack/Software.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hack;
+
+/**
+ *
+ * @author Dima
+ */
+public class Software {
+    
+    String name,desc;
+    
+    int price,version,type;
+    
+}
