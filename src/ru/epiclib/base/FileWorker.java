0abd29a33b24cd977e3dfe97955f878e6b0dab4b
Dmig <Dmig13@ya.ru>
2016-05-30 14:52:58 +0300

    Deleted bad comments

--- src/ru/epiclib/base/FileWorker.java
+++ src/ru/epiclib/base/FileWorker.java
@@ -5,24 +5,18 @@ import java.io.*;
 public class FileWorker {
 
     public static void write(String fileName, String text) {
-        //РћРїСЂРµРґРµР»СЏРµРј С„Р°Р№Р»
         File file = new File(fileName);
 
         try {
-            //РїСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ РµСЃР»Рё С„Р°Р№Р» РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚ С‚Рѕ СЃРѕР·РґР°РµРј РµРіРѕ
             if (!file.exists()) {
                 file.createNewFile();
             }
 
-            //PrintWriter РѕР±РµСЃРїРµС‡РёС‚ РІРѕР·РјРѕР¶РЅРѕСЃС‚Рё Р·Р°РїРёСЃРё РІ С„Р°Р№Р»
             PrintWriter out = new PrintWriter(file.getAbsoluteFile());
 
             try {
-                //Р—Р°РїРёСЃС‹РІР°РµРј С‚РµРєСЃС‚ Сѓ С„Р°Р№Р»
                 out.print(text);
             } finally {
-                //РџРѕСЃР»Рµ С‡РµРіРѕ РјС‹ РґРѕР»Р¶РЅС‹ Р·Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
-                //Р�РЅР°С‡Рµ С„Р°Р№Р» РЅРµ Р·Р°РїРёС€РµС‚СЃСЏ
                 out.close();
             }
         } catch (IOException e) {
@@ -32,20 +26,15 @@ public class FileWorker {
 
     public static void write(File file, String text) {
         try {
-            //РїСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ РµСЃР»Рё С„Р°Р№Р» РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚ С‚Рѕ СЃРѕР·РґР°РµРј РµРіРѕ
             if (!file.exists()) {
                 file.createNewFile();
             }
 
-            //PrintWriter РѕР±РµСЃРїРµС‡РёС‚ РІРѕР·РјРѕР¶РЅРѕСЃС‚Рё Р·Р°РїРёСЃРё РІ С„Р°Р№Р»
             PrintWriter out = new PrintWriter(file.getAbsoluteFile());
 
             try {
-                //Р—Р°РїРёСЃС‹РІР°РµРј С‚РµРєСЃС‚ Сѓ С„Р°Р№Р»
                 out.print(text);
             } finally {
-                //РџРѕСЃР»Рµ С‡РµРіРѕ РјС‹ РґРѕР»Р¶РЅС‹ Р·Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
-                //Р�РЅР°С‡Рµ С„Р°Р№Р» РЅРµ Р·Р°РїРёС€РµС‚СЃСЏ
                 out.close();
             }
         } catch (IOException e) {
@@ -55,24 +44,19 @@ public class FileWorker {
 
     public static String read(String fileName) throws FileNotFoundException {
 
-        //РћРїСЂРµРґРµР»СЏРµРј С„Р°Р№Р»
         File file = new File(fileName);
 
-        //Р­С‚РѕС‚ СЃРїРµС†. РѕР±СЉРµРєС‚ РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ СЃС‚СЂРѕРєРё
         StringBuilder sb = new StringBuilder();
         try {
             try {
-                //РћР±СЉРµРєС‚ РґР»СЏ С‡С‚РµРЅРёСЏ С„Р°Р№Р»Р° РІ Р±СѓС„РµСЂ
                 BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
                 try {
-                    //Р’ С†РёРєР»Рµ РїРѕСЃС‚СЂРѕС‡РЅРѕ СЃС‡РёС‚С‹РІР°РµРј С„Р°Р№Р»
                     String s;
                     while ((s = in.readLine()) != null) {
                         sb.append(s);
                         sb.append("\n");
                     }
                 } finally {
-                    //РўР°РєР¶Рµ РЅРµ Р·Р°Р±С‹РІР°РµРј Р·Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
                     in.close();
                 }
             } catch (FileNotFoundException e) {
@@ -82,27 +66,22 @@ public class FileWorker {
             System.exit(1);
         }
 
-        //Р’РѕР·РІСЂР°С‰Р°РµРј РїРѕР»СѓС‡РµРЅРЅС‹Р№ С‚РµРєСЃС‚ СЃ С„Р°Р№Р»Р°
         return sb.toString();
     }
 
     public static String read(File file) throws FileNotFoundException {
 
-        //Р­С‚РѕС‚ СЃРїРµС†. РѕР±СЉРµРєС‚ РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ СЃС‚СЂРѕРєРё
         StringBuilder sb = new StringBuilder();
         try {
             try {
-                //РћР±СЉРµРєС‚ РґР»СЏ С‡С‚РµРЅРёСЏ С„Р°Р№Р»Р° РІ Р±СѓС„РµСЂ
                 BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
                 try {
-                    //Р’ С†РёРєР»Рµ РїРѕСЃС‚СЂРѕС‡РЅРѕ СЃС‡РёС‚С‹РІР°РµРј С„Р°Р№Р»
                     String s;
                     while ((s = in.readLine()) != null) {
                         sb.append(s);
                         sb.append("\n");
                     }
                 } finally {
-                    //РўР°РєР¶Рµ РЅРµ Р·Р°Р±С‹РІР°РµРј Р·Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
                     in.close();
                 }
             } catch (FileNotFoundException e) {
@@ -112,30 +91,24 @@ public class FileWorker {
             System.exit(1);
         }
 
-        //Р’РѕР·РІСЂР°С‰Р°РµРј РїРѕР»СѓС‡РµРЅРЅС‹Р№ С‚РµРєСЃС‚ СЃ С„Р°Р№Р»Р°
         return sb.toString();
     }
 
     public static String read(String fileName, String record) throws FileNotFoundException {
 
-        //РћРїСЂРµРґРµР»СЏРµРј С„Р°Р№Р»
         File file = new File(fileName);
 
-        //Р­С‚РѕС‚ СЃРїРµС†. РѕР±СЉРµРєС‚ РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ СЃС‚СЂРѕРєРё
         StringBuilder sb = new StringBuilder();
         try {
             try {
-                //РћР±СЉРµРєС‚ РґР»СЏ С‡С‚РµРЅРёСЏ С„Р°Р№Р»Р° РІ Р±СѓС„РµСЂ
                 BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
                 try {
-                    //Р’ С†РёРєР»Рµ РїРѕСЃС‚СЂРѕС‡РЅРѕ СЃС‡РёС‚С‹РІР°РµРј С„Р°Р№Р»
                     String s;
                     while ((s = in.readLine()) != null) {
                         sb.append(s);
                         sb.append("\n");
                     }
                 } finally {
-                    //РўР°РєР¶Рµ РЅРµ Р·Р°Р±С‹РІР°РµРј Р·Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
                     in.close();
                 }
             } catch (FileNotFoundException e) {
@@ -146,26 +119,21 @@ public class FileWorker {
             System.exit(1);
         }
 
-        //Р’РѕР·РІСЂР°С‰Р°РµРј РїРѕР»СѓС‡РµРЅРЅС‹Р№ С‚РµРєСЃС‚ СЃ С„Р°Р№Р»Р°
         return sb.toString();
     }
 
     public static String read(File file, String record) throws FileNotFoundException {
-        //Р­С‚РѕС‚ СЃРїРµС†. РѕР±СЉРµРєС‚ РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ СЃС‚СЂРѕРєРё
         StringBuilder sb = new StringBuilder();
         try {
             try {
-                //РћР±СЉРµРєС‚ РґР»СЏ С‡С‚РµРЅРёСЏ С„Р°Р№Р»Р° РІ Р±СѓС„РµСЂ
                 BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
                 try {
-                    //Р’ С†РёРєР»Рµ РїРѕСЃС‚СЂРѕС‡РЅРѕ СЃС‡РёС‚С‹РІР°РµРј С„Р°Р№Р»
                     String s;
                     while ((s = in.readLine()) != null) {
                         sb.append(s);
                         sb.append("\n");
                     }
                 } finally {
-                    //РўР°РєР¶Рµ РЅРµ Р·Р°Р±С‹РІР°РµРј Р·Р°РєСЂС‹С‚СЊ С„Р°Р№Р»
                     in.close();
                 }
             } catch (FileNotFoundException e) {
@@ -176,7 +144,6 @@ public class FileWorker {
             System.exit(1);
         }
 
-        //Р’РѕР·РІСЂР°С‰Р°РµРј РїРѕР»СѓС‡РµРЅРЅС‹Р№ С‚РµРєСЃС‚ СЃ С„Р°Р№Р»Р°
         return sb.toString();
     }
 
